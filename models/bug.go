ffc9ae463b3fe0af7fbb4ef93a9973bf9ae39648
1rvyn <i.hall3@rgu.ac.uk>
2023-02-24 12:11:17 +0000

    make each bug report unique with pk + index

--- models/bug.go
+++ models/bug.go
@@ -1,7 +1,8 @@
 package models
 
 type Bug struct {
-	Email string `json:"email" gorm:"primaryKey" gorm:"unique;unique_index" `
+	ID    uint   `json:"id" gorm:"primaryKey" gorm:"unique;unique_index"`
+	Email string `json:"email"`
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
