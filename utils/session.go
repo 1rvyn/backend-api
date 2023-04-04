97c1d71a037fd3dde05fa42ce73fda06484eb04a
1rvyn <i.hall3@rgu.ac.uk>
2023-04-04 22:46:39 +0100

    revert + change

--- utils/session.go
+++ utils/session.go
@@ -13,19 +13,28 @@ var SecretKey = os.Getenv("JWT_SECRET")
 
 func GetSession(c *fiber.Ctx) (map[string]string, error) {
 
-	authHeader := c.Get("Authorization")
-	if authHeader == "" {
-		return nil, errors.New("no JWT found in the Authorization header")
+	cookieHeader := c.Cookies("Cookie")
+	if cookieHeader == "" {
+		return nil, errors.New("no JWT found in the Cookie header")
 	}
 
-	if !strings.HasPrefix(authHeader, "Bearer ") {
-		return nil, errors.New("invalid Authorization header format")
+	// Extract the jwt cookie from the cookie header
+	jwtCookie := ""
+	cookies := strings.Split(cookieHeader, ";")
+	for _, cookie := range cookies {
+		if strings.HasPrefix(strings.TrimSpace(cookie), "jwt=") {
+			jwtCookie = strings.TrimSpace(strings.TrimPrefix(cookie, "jwt="))
+			break
+		}
 	}
 
-	cookie := strings.TrimPrefix(authHeader, "Bearer ")
+	fmt.Println("new cookie format is: ", jwtCookie)
+	if jwtCookie == "" {
+		return nil, errors.New("no JWT cookie found")
+	}
 
-	fmt.Println("cookie is: ", cookie)
-	claims, err := GetClaimsFromCookie(cookie, SecretKey)
+	fmt.Println("cookie is: ", jwtCookie)
+	claims, err := GetClaimsFromCookie(jwtCookie, SecretKey)
 	if err != nil {
 		return nil, err
 	}
