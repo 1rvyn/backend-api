b1bc95673cc73166ff7c4c8ddc6204e2e5b7f9e0
1rvyn <i.hall3@rgu.ac.uk>
2023-04-04 22:50:16 +0100

    fix?

--- utils/session.go
+++ utils/session.go
@@ -6,35 +6,19 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"os"
-	"strings"
 )
 
 var SecretKey = os.Getenv("JWT_SECRET")
 
 func GetSession(c *fiber.Ctx) (map[string]string, error) {
 
-	cookieHeader := c.Cookies("Cookie")
-	if cookieHeader == "" {
-		return nil, errors.New("no JWT found in the Cookie header")
+	jwt := c.Get("X-JWT")
+	if jwt == "" {
+		return nil, errors.New("no JWT found in the X-JWT header")
 	}
 
-	// Extract the jwt cookie from the cookie header
-	jwtCookie := ""
-	cookies := strings.Split(cookieHeader, ";")
-	for _, cookie := range cookies {
-		if strings.HasPrefix(strings.TrimSpace(cookie), "jwt=") {
-			jwtCookie = strings.TrimSpace(strings.TrimPrefix(cookie, "jwt="))
-			break
-		}
-	}
-
-	fmt.Println("new cookie format is: ", jwtCookie)
-	if jwtCookie == "" {
-		return nil, errors.New("no JWT cookie found")
-	}
-
-	fmt.Println("cookie is: ", jwtCookie)
-	claims, err := GetClaimsFromCookie(jwtCookie, SecretKey)
+	fmt.Println("X-JWT is: ", jwt)
+	claims, err := GetClaimsFromCookie(jwt, SecretKey)
 	if err != nil {
 		return nil, err
 	}
