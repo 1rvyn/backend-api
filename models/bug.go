e6773c291c110a99921d726862862d0c0ea87018
1rvyn <i.hall3@rgu.ac.uk>
2023-04-25 15:46:21 +0100

    Body pkey

--- models/bug.go
+++ models/bug.go
@@ -1,10 +1,8 @@
 package models
 
 type Bug struct {
-	ID    uint   `json:"id" gorm:"primaryKey" gorm:"unique;unique_index"`
+	ID    uint   `json:"id"`
 	Email string `json:"email"`
 	Title string `json:"title"`
-	Body  string `json:"body"`
+	Body  string `json:"body" gorm:"primaryKey"`
 }
-
-// TODO: Rate limit the fuck out of this endpoint (throw back)
